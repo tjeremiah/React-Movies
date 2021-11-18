15f2a864ec6b799a1e1edb7fb42468c73d6440fb
Torrel <taurel107@gmail.com>
2021-11-17 20:58:43 -0500

    added config file and components to the component folder

---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+// API
+import API from '../API';
+
+ 
+// Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from '../config'
+
+
+
+
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+    const [state, setState] = useState(); //hold all the movies
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false)
+
+    const fetchMovies = async (page, searchTerm = "") => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const movies = await API.fetchMovies(searchTerm, page);
+            console.log(movies)
+
+            setState(prev => ({
+                ...movies, 
+                results:
+                 page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+            }));
+
+        } catch (error) {
+            setError(true)
+        }
+        setLoading(false)
+
+    };
+
+    // Initial render
+    useEffect(() => {
+        fetchMovies(1)
+    }, [])
+    
+    console.log(state);
+    return <div>Home Page</div>
+}
+
+export default Home;
